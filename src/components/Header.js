5fd24e227d8b157bc428ffe300016e6e288aae65
sparrow313 <98008251+sparrow313@users.noreply.github.com>
2022-04-22 01:43:48 +0530

    header done

---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import {
+  BarChart,
+  SearchRounded,
+  ShoppingCartRounded,
+} from "@mui/icons-material";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Flogo.png?alt=media&token=fc228623-ef27-4af4-8ea5-b9ebeeaf47dc"
+        alt=""
+        className="logo"
+      />
+
+      <div className="inputBox">
+        <SearchRounded className="searchIcon" />
+        <input type="text" placeholder="search" />
+      </div>
+      <div className="shoppingCart">
+        <ShoppingCartRounded className="cart" />
+        <div className="cart_content">
+          <p>2</p>
+        </div>
+      </div>
+      <div className="profileContainer">
+        <div className="imgBox">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fprofile.jpg?alt=media&token=36821495-39b9-4145-bde3-16c47c6ff937"
+            alt=""
+          />
+        </div>
+      </div>
+      <h2 className="userName">Javed</h2>
+      <div className="toggleMenu">
+        <BarChart className="toggleIcon" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
